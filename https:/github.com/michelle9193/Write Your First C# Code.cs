0d2b00e86fe62b3fb8a7d8c86f1345e6196f571d
michellefortuin93 <michellefortuin93@gmail.com>
2024-01-23 15:28:49 +0200

    Challenge 1

---
+++ https:/github.com/michelle9193/Write Your First C# Code.cs
@@ -0,0 +1,24 @@
+/*
+Learning objectives
+After you complete this module, you'll be able to:
+
+Write your first lines of C# code
+Use two different techniques to print a message to a text console
+Diagnose errors when you type code incorrectly
+Identify different C# syntax elements like operators, classes, and methods
+*/
+
+/*
+Challenge: Write code in the .NET Editor to display two messages
+*/
+
+Console.WriteLine("This is the first line");
+Console.WriteLine("This is the second line");
+
+/* ANSWER
+Console.WriteLine("This is the first line.");
+
+Console.Write("This is ");
+Console.Write("the second ");
+Console.Write("line.");
+*/
