1c4cfbdb40bd8af920816187f99210ad8cdaf5e8
michellefortuin93 <michellefortuin93@gmail.com>
2024-01-24 12:26:15 +0200

    add exercise code to #4

---
+++ 4.Perform Basic Operations on Numbers in C#.cs
@@ -0,0 +1,14 @@
+/*
+Learn the operators and techniques used to perform basic math operations on numeric data.
+
+Learning objectives
+After you complete this module, you'll be able to:
+
+Perform mathematical operations on numeric values
+Observe implicit type conversion between strings and numeric values
+Temporarily convert one data type into another
+*/
+
+int fahrenheit = 94;
+decimal celcius = (fahrenheit - 32.0m) * (5.0m / 9.0m);
+Console.WriteLine($"The temperature is {celcius} Celcius");
