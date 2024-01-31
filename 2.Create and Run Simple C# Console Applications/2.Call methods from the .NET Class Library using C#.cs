c696d72deb51c82182c745815e863f0e2e35e843
michellefortuin93 <michellefortuin93@gmail.com>
2024-01-31 19:08:45 +0200

    add module 2

---
+++ 2.Create and Run Simple C# Console Applications/2.Call methods from the .NET Class Library using C#.cs
@@ -0,0 +1,12 @@
+/*
+Use functionality in the .NET Class Library by calling methods that return values, accept input parameters, and more.
+
+Learning objectives
+In this module, you will:
+
+Write code that calls stateless methods in the .NET Class Library.
+Create a new instance of .NET Class Library classes to call methods that maintain state.
+Use IntelliSense to learn more about a method, its overloaded versions, its return value data type, and its input parameter data types.
+Use Microsoft Learn documentation to research what a method does, its overloaded versions, its return value type, its input parameters and what each parameter represents.
+
+*/
