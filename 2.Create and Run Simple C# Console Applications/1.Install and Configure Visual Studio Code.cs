994297a5f4dadc80c3130f4fff5313245ed09da1
michellefortuin93 <michellefortuin93@gmail.com>
2024-01-31 17:59:24 +0200

    add module information

---
+++ 2.Create and Run Simple C# Console Applications/1.Install and Configure Visual Studio Code.cs
@@ -0,0 +1,10 @@
+/* Install and configure Visual Studio Code
+
+Learn how to configure Visual Studio Code for C# and how to use this professional IDE to create and run console applications.
+
+Learning objectives
+In this module, you will:
+Configure Visual Studio Code and your development environment for programming in C#.
+Explore the Visual Studio Code user interface.
+Create, edit, build, and run a console application using Visual Studio Code.
+*/
